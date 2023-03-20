4baf1465661a9ec658faf2edcb3d656060885dfb
reader00 <qoryamanahputra0708@gmail.com>
2023-03-20 17:01:52 +0700

    transactions

--- src/state/api.js
+++ src/state/api.js
@@ -5,17 +5,29 @@ export const api = createApi({
         baseUrl: process.env.REACT_APP_BASE_URL,
     }),
     reducerPath: 'adminApi',
-    tagTypes: ['User', 'Products'],
+    tagTypes: ['User', 'Products', 'Customers', 'Transactions'],
     endpoints: (build) => ({
         getUser: build.query({
             query: (id) => `general/user/${id}`,
             providesTags: ['User'],
         }),
         getProducts: build.query({
-            query: (id) => `client/products`,
+            query: () => `client/products`,
             providesTags: ['Products'],
+        }),
+        getCustomers: build.query({
+            query: () => `client/customers`,
+            providesTags: ['Customers'],
+        }),
+        getTransactions: build.query({
+            query: ({ page, pageSize, sort, search }) => ({
+                url: `client/transactions`,
+                method: 'GET',
+                params: { page, pageSize, sort, search },
+            }),
+            providesTags: ['Transactions'],
         }),
     }),
 });
 
-export const { useGetUserQuery, useGetProductsQuery } = api;
+export const { useGetUserQuery, useGetProductsQuery, useGetCustomersQuery, useGetTransactionsQuery } = api;
